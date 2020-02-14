cc630696e5d37565e92b1fcee42eeec4928e5454
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-14 14:01:16 +0700

    employee add complete

--- Angular/angular-casestudy/src/app/components/customers/customer-add/customer-add.component.ts
+++ Angular/angular-casestudy/src/app/components/customers/customer-add/customer-add.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CustomerService} from '../../../services/customer.service';
+import {Router} from '@angular/router';
+interface CustomerType {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-customer-add',
   templateUrl: './customer-add.component.html',
   styleUrls: ['./customer-add.component.scss']
 })
 export class CustomerAddComponent implements OnInit {
+  public formAddNewCustomer: FormGroup;
+  public maxDate = new Date();
+  public minDate = new Date(1900, 0, 1);
+  customerType: CustomerType[] = [
+    {value: 'diamond', viewValue: 'Diamond'},
+    {value: 'platinum', viewValue: 'Platinum'},
+    {value: 'gold', viewValue: 'Gold'},
+    {value: 'silver', viewValue: 'Silver'},
+    {value: 'member', viewValue: 'Member'}
+  ];
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formAddNewCustomer = this.formBuilder.group({
+      fullName: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      identityNumber: ['', [Validators.required, Validators.pattern('^\\d{9}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^(?:\\(84\\)\\+|0)9[01]\\d{7}$')]],
+      address: ['', [Validators.required]]
+    });
   }
 
+  addCustomer() {
+    // this.customerService.addNewCustomer(this.formAddNewCustomer.value).subscribe(data => {
+    //     //   this.router.navigateByUrl('customer-list');
+    //     // });
+    console.log(this.formAddNewCustomer.value);
+  }
 }
