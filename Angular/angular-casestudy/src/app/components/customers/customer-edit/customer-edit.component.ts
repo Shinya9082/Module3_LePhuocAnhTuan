3108d268c3ca8fdfe9f1d49c2a6a31f63b112e7c
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-14 15:19:43 +0700

    customer-edit.component completed finish customer module

--- Angular/angular-casestudy/src/app/components/customers/customer-edit/customer-edit.component.ts
+++ Angular/angular-casestudy/src/app/components/customers/customer-edit/customer-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CustomerService} from '../../../services/customer.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-customer-edit',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-edit.component.scss']
 })
 export class CustomerEditComponent implements OnInit {
+  public formEditCustomer: FormGroup;
+  public maxDate = new Date();
+  public minDate = new Date(1900, 0, 1);
+  public customerId;
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router,
+    public activatedRoute: ActivatedRoute
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formEditCustomer = this.formBuilder.group({
+      fullName: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      identityNumber: ['', [Validators.required, Validators.pattern('^\\d{9}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^(?:\\(84\\)\\+|0)9[01]\\d{7}$')]],
+      address: ['', [Validators.required]]
+    });
+    this.activatedRoute.params.subscribe(data => {
+      this.customerId = data.id;
+      this.customerService.getCustomerById(this.customerId).subscribe(data => {
+        this.formEditCustomer.patchValue(data);
+      })
+    })
   }
 
+  editCustomer() {
+    this.customerService.editCustomer(this.formEditCustomer.value,this.customerId).subscribe(data =>{
+      this.router.navigateByUrl('customer-list')
+    });
+  }
 }
