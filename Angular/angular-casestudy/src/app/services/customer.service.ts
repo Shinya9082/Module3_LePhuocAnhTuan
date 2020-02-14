81d4c3b13f78d2347de880c50cd7fc7987d839ba
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-14 10:06:11 +0700

    customer.service.ts complete

--- Angular/angular-casestudy/src/app/services/customer.service.ts
+++ Angular/angular-casestudy/src/app/services/customer.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-
-  constructor() { }
+  public readonly apiUrl = 'http://localhost:3000/customers';
+  constructor(private http: HttpClient) { }
+  getAllCustomer(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+  addNewCustomer(customer): Observable<any> {
+    return this.http.post(this.apiUrl, customer);
+  }
+  getCustomerById(id: number): Observable<any> {
+    return this.http.get(this.apiUrl + '/' + id);
+  }
+  deleteCustomerById(id: number): Observable<any> {
+    return this.http.delete(this.apiUrl + '/' + id);
+  }
+  editCustomer(customer, customerId): Observable<any> {
+    return this.http.put(this.apiUrl + '/' + customerId, customer);
+  }
 }
