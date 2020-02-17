e4e972aa4d1b871c997e001771c6922b9fa5e7a3
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-17 10:02:27 +0700

    service-edit.component completed

--- Angular/angular-casestudy/src/app/components/services/service-edit/service-edit.component.ts
+++ Angular/angular-casestudy/src/app/components/services/service-edit/service-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ServiceService} from '../../../services/service.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-service-edit',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-edit.component.scss']
 })
 export class ServiceEditComponent implements OnInit {
+  public formEditService: FormGroup;
+  public serviceId;
+  constructor(
+    private formBuilder: FormBuilder,
+    private serviceService: ServiceService,
+    private router: Router,
+    public activatedRoute: ActivatedRoute
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formEditService = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      area: ['', [Validators.required, Validators.pattern('^\\d+$')]],
+      floor: ['', [Validators.required, Validators.pattern('^\\d+$')]],
+      maxPeople: ['', [Validators.required, Validators.pattern('^\\d+$')]],
+      price: ['', [Validators.required, Validators.pattern('^\\d+$')]],
+      rentType: ['', [Validators.required]],
+      status: ['', [Validators.required]]
+    });
+    this.activatedRoute.params.subscribe(data => {
+      this.serviceId = data.id;
+      this.serviceService.getServiceById(this.serviceId).subscribe(data => {
+        this.formEditService.patchValue(data);
+      })
+    })
   }
-
+  editService() {
+    this.serviceService.editService(this.formEditService.value, this.serviceId ).subscribe(data => {
+      this.router.navigateByUrl('service-list');
+    });
+  }
 }
