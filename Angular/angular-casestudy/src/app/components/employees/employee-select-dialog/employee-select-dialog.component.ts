9278855a497df938d157d3eb1bce1bc32a77b8f2
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-17 15:23:34 +0700

    employee-select-dialog.componen completed added into order-add.component.htmt

--- Angular/angular-casestudy/src/app/components/employees/employee-select-dialog/employee-select-dialog.component.ts
+++ Angular/angular-casestudy/src/app/components/employees/employee-select-dialog/employee-select-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {EmployeeService} from '../../../services/employee.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-employee-select-dialog',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-select-dialog.component.scss']
 })
 export class EmployeeSelectDialogComponent implements OnInit {
+  employeeId;
+  displayedColumns: string[] = ['id', 'fullName', 'choose'];
+  dataSource;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public employeeService: EmployeeService,
+    public dialogRef: MatDialogRef<EmployeeSelectDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+    this.employeeService.getAllEmployee().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  selectEmployee(element: any) {
+
+  }
 }
--- Angular/angular-casestudy/src/app/components/orders/order-add/order-add.component.ts
+++ Angular/angular-casestudy/src/app/components/orders/order-add/order-add.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {OrderService} from '../../../services/order.service';
+import {Router} from '@angular/router';
+import {EmployeeService} from '../../../services/employee.service';
+import {CustomerService} from '../../../services/customer.service';
+import {ServiceService} from '../../../services/service.service';
+import {EmployeeSelectDialogComponent} from '../../employees/employee-select-dialog/employee-select-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-order-add',
@@ -6,10 +14,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-add.component.scss']
 })
 export class OrderAddComponent implements OnInit {
-
-  constructor() { }
+  public formAddNewOrder: FormGroup;
+  public order;
+  public employee;
+  public minDate = new Date();
+  constructor(
+    private formBuilder: FormBuilder,
+    private orderService: OrderService,
+    private employeeService: EmployeeService,
+    private customerService: CustomerService,
+    private serviceService: ServiceService,
+    public dialog: MatDialog,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.formAddNewOrder = this.formBuilder.group({
+      employee: ['', [Validators.required]],
+      customer: ['', [Validators.required]],
+      service: ['', [Validators.required]],
+      dateStart: ['', [Validators.required]],
+      dateEnd: ['', [Validators.required]],
+      deposits: ['', [Validators.required]],
+      total: ['', [Validators.required]]
+    });
   }
+  openEmployeeDialog(): void {
+    const dialogRef = this.dialog.open(EmployeeSelectDialogComponent, {
+      width: '50%',
+      position: {top: '10%'},
+      data: {employee: this.employee}
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+      this.employee = result;
+      }
+      console.log(this.employee);
+    });
+  }
 }
