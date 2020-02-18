69d0e2ee5e434b76bea167ef80d831f1ab1f5e78
Shinya9082 <anhtuandtu1993@gmail.com>
2020-02-18 11:23:45 +0700

    orders-detail-add + list completed

--- Angular/angular-casestudy/src/app/components/oders-detail/orders-detail-list/orders-detail-list.component.ts
+++ Angular/angular-casestudy/src/app/components/oders-detail/orders-detail-list/orders-detail-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {OrderService} from '../../../services/order.service';
+import {OrderDetailService} from '../../../services/order-detail.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-orders-detail-list',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders-detail-list.component.scss']
 })
 export class OrdersDetailListComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['id', 'order', 'attachService', 'amount'];
+  dataSource;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  constructor(
+    public orderDetailService: OrderDetailService
+  ) { }
 
   ngOnInit(): void {
+    this.orderDetailService.getAllOrderDetail().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
 }
